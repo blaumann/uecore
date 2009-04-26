68ed3feab79e7df7455461b4a0f07f6c61912965
Thyros <trainzfreak@gmx.de>
2009-04-26 15:04:04 +0200

    - Updated the SQL Files, thanks to Fabian.

--- sql/fixes/auto-announcer/aa.sql
+++ sql/fixes/auto-announcer/aa.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `autobroadcast` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
 INSERT INTO `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
-(1200, '|cffffcc00[Server]: |cff00ff00%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(1202, '|cffffcc00[Server]: |cff00ff00%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
