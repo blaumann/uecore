0aa5543c7f96dbc12c0a35bbbf6116d8526b3d5a
Thyros <trainzfreak@gmx.de>
2009-05-07 01:18:31 +0200

    - Attempt to fix quests. Some other fixes.

--- src/game/GossipDef.cpp
+++ src/game/GossipDef.cpp
@@ -769,8 +769,7 @@ void PlayerMenu::SendQuestGiverRequestItems( Quest const *pQuest, uint64 npcGUID
         }
     }
 
-    // We may wish a better check, perhaps checking the real quest requirements
-    if (RequestItemsText.empty())
+    if (!pQuest->GetReqItemsCount() && Completable)
     {
         SendQuestGiverOfferReward(pQuest, npcGUID, true);
         return;
