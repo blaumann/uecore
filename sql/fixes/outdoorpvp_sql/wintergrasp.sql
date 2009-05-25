12eeee8cf9276e8e28269bb7801aa12edad7fe94
Thyros <trainzfreak@gmx.de>
2009-05-25 17:12:29 +0200

    - Fixed wintergrasp sql

--- sql/fixes/outdoorpvp_sql/wintergrasp.sql
+++ sql/fixes/outdoorpvp_sql/wintergrasp.sql
@@ -10,10 +10,4 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (61408, 1, 27852);
 
 
-update creature_template set maxhealth = 133525, minhealth = 133525, maxmana = 51360, minmana = 51360, spell1 = 53114, spell2 = 53112, spell3=53110 where entry = 28670;
-update creature_template set maxhealth = 30000, minhealth = 30000, speed = 2, spell1=50025, spell2=50989, VehicleId = 36 where entry = 27881;
-update creature_template set maxhealth = 50000, minhealth = 50000, speed = 1.6, spell1=50896, spell2=50652, VehicleId = 106 where entry = 28094;
-update creature_template set maxhealth = 75000, minhealth = 75000, speed = 1, spell1=51678, VehicleId = 117 where entry IN (28312,32627);
-update creature_template set maxhealth = 50000, minhealth = 50000, spell1=51362, VehicleId = 116 where entry IN (28319,32629);
-
 update creature_template set scriptname = "npc_demolisher_engineerer" where entry in (30400,30499);
