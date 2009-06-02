dd18c65fe8af5bf4fae5f47b036afa483b9421ce
Filipper <mtbdriver@gmx.de>
2009-06-02 11:36:16 +0200

    - Fixed boss_urom.cpp in Oculus

--- src/bindings/ScriptDev2/scripts/zone/nexus/oculus/boss_urom.cpp
+++ src/bindings/ScriptDev2/scripts/zone/nexus/oculus/boss_urom.cpp
@@ -1,48 +1,94 @@
 /* Script Data Start
 SDName: Boss urom
-SDAuthor: LordVanMartin
-SD%Complete: 
+SDAuthor: LordVanMartin / Edited by Filipper
+SD%Complete: Timers may be not blizzlike
 SDComment: 
 SDCategory: 
 Script Data End */
 
 /*** SQL START *** 
-update creature_template set scriptname = '' where entry = '';
+update creature_template set scriptname = 'boss_urom' where entry = '27655';
 *** SQL END ***/
 #include "precompiled.h"
 
 //Spells
-#define SPELL_ARCANE_SHIELD                      53813 //Dummy --> Channeled, shields the caster from damage.
-#define SPELL_EMPOWERED_ARCANE_EXPLOSION         51110
-#define SPELL_EMPOWERED_ARCANE_EXPLOSION_2       59377
-#define SPELL_FROSTBOMB                          51103
-#define SPELL_SUMMON_MENAGERIE                   50476 //Summons an assortment of creatures and teleports the caster to safety.
-#define SPELL_SUMMON_MENAGERIE_2                 50495
-#define SPELL_SUMMON_MENAGERIE_3                 50496
-#define SPELL_TELEPORT                           51112 //Teleports to the center of Oculus
-#define SPELL_TIME_BOMB                          51121
-#define SPELL_TIME_BOMB_2                        59376
+enum Spells
+{
+	SPELL_ARCANE_SHIELD						= 53813, //Dummy --> Channeled, shields the caster from damage.
+	SPELL_EMPOWERED_ARCANE_EXPLOSION_N		= 51110, //Normal
+	SPELL_EMPOWERED_ARCANE_EXPLOSION_H		= 59377, //Heroic
+	SPELL_FROSTBOMB							= 51103,
+	SPELL_SUMMON_MENAGERIE					= 50476, //Summons an assortment of creatures and teleports the caster to safety.
+	SPELL_SUMMON_MENAGERIE_2				= 50495,
+	SPELL_SUMMON_MENAGERIE_3				= 50496,
+	SPELL_TELEPORT							= 51112, //Teleports to the center of Oculus
+	SPELL_TIME_BOMB_N						= 51121, //Normal
+	SPELL_TIME_BOMB_H						= 59376  //Heroic
+};
 
-//Yell
-#define SAY_AGGRO                             -1999757
-#define SAY_KILL_1                            -1999756 
-#define SAY_KILL_2                            -1999755 
-#define SAY_KILL_3                            -1999754 
-#define SAY_DEATH                             -1999753
-#define SAY_EXPLOSION_1                       -1999752      
-#define SAY_EXPLOSION_2                       -1999751      
-#define SAY_SUMMON_1                          -1999750   
-#define SAY_SUMMON_2                          -1999739   
-#define SAY_SUMMON_3                          -1999748   
+enum Yell
+{
+	SAY_AGGRO			= -1999757,
+	SAY_KILL_1			= -1999756,
+	SAY_KILL_2			= -1999755,
+	SAY_KILL_3			= -1999754,
+	SAY_DEATH			= -1999753,
+	SAY_EXPLOSION_1		= -1999752,
+	SAY_EXPLOSION_2		= -1999751,
+	SAY_SUMMON_1		= -1999750,
+	SAY_SUMMON_2		= -1999739,
+	SAY_SUMMON_3		= -1999748
+};  
 
 struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
 {
-    boss_uromAI(Creature* pCreature) : ScriptedAI(pCreature) {Reset();}
+    boss_uromAI(Creature* pCreature) : ScriptedAI(pCreature)
+	{
+		Reset();
+		HeroicMode = m_creature->GetMap()->IsHeroic();
+	}
 
-    void Reset() {}
+	bool HeroicMode;
+
+	uint32 ShieldTimer;
+	uint32 FrostbombTimer;
+	uint32 TeleportTimer;
+	uint32 ExplosionTimer;
+	uint32 TimebombTimer;
+
+	uint32 Attacked;
+
+    void Reset() 
+	{
+		FrostbombTimer = 8000; // Timer may be not blizzlike
+		TeleportTimer = 45000; // Timer may be not blizzlike
+		TimebombTimer = 25000; // Timer may be not blizzlike
+
+		Attacked = 0;
+	}
     void Aggro(Unit* who) 
     {
-        DoScriptText(SAY_AGGRO, m_creature);
+		if (Attacked == 3)
+			DoScriptText(SAY_AGGRO, m_creature);
+		if (Attacked < 3)
+		{
+			if (Attacked == 2)
+			{
+				m_creature->CastSpell(m_creature, SPELL_SUMMON_MENAGERIE_3, true);
+				DoScriptText(SAY_SUMMON_1, m_creature);
+			}
+			if (Attacked == 1)
+			{
+				m_creature->CastSpell(m_creature, SPELL_SUMMON_MENAGERIE, true);
+				DoScriptText(SAY_SUMMON_2, m_creature);
+			}
+			if (Attacked == 0)
+			{
+				m_creature->CastSpell(m_creature, SPELL_SUMMON_MENAGERIE, true);
+				DoScriptText(SAY_SUMMON_3, m_creature);
+			}
+			Attacked++;
+		}
     }
     void AttackStart(Unit* who) {}
     void MoveInLineOfSight(Unit* who) {}
@@ -51,6 +97,45 @@ struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
         //Return since we have no target
         if (!m_creature->SelectHostilTarget() || !m_creature->getVictim())
             return;
+
+		if (Attacked == 3)
+		{
+			if (FrostbombTimer < diff)
+			{
+				m_creature->CastSpell(m_creature->getVictim(), SPELL_FROSTBOMB, true);
+				FrostbombTimer = 8000;
+			}else FrostbombTimer -=diff;
+
+			if (TeleportTimer < diff)
+			{
+				m_creature->CastSpell(m_creature, SPELL_TELEPORT, true);
+				m_creature->CastSpell(m_creature, SPELL_ARCANE_SHIELD, true);
+				ExplosionTimer = 2500;
+				if (ExplosionTimer < diff)
+				{
+					if (HeroicMode)
+						m_creature->CastSpell(m_creature, SPELL_EMPOWERED_ARCANE_EXPLOSION_H, true);
+					else m_creature->CastSpell(m_creature, SPELL_EMPOWERED_ARCANE_EXPLOSION_N, true); 
+					switch(rand()%3)
+					{
+						case 0: DoScriptText(SAY_EXPLOSION_1, m_creature); break;
+						case 1: DoScriptText(SAY_EXPLOsion_2, m_creature); break;
+					}
+				}else ExplosionTimer -=diff;
+			}
+
+			if (TimebombTimer < diff)
+			{
+				if (Unit *target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				{
+					if (HeroicMode)
+						m_creature->CastSpell(target, SPELL_TIME_BOMB_H, true);
+					else m_creature->CastSpell(target, SPELL_TIME_BOMB_N, true);
+				}
+				TimebombTimer = 20000;
+			}else TimebombTimer -=diff;
+
+		}
                 
         DoMeleeAttackIfReady();    
     }
