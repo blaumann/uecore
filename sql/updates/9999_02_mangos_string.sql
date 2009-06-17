b7d6f041fa1545ce8a3280d353b7e2c5717d34a3
Sanzzes <stevenbohm@sanzzes-city.com>
2009-06-17 13:55:42 +0200

    - Cleanup Restart Source

--- sql/updates/9999_02_mangos_string.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM `mangos_string` WHERE (`entry`='747');
-DELETE FROM `mangos_string` WHERE (`entry`='748');
-DELETE FROM `mangos_string` WHERE (`entry`='749');
-DELETE FROM `mangos_string` WHERE (`entry`='750');
-INSERT INTO `mangos_string` (`entry`,`content_default`) VALUES ('747','%s traveled %u seconds into the future.');
-INSERT INTO `mangos_string` (`entry`,`content_default`) VALUES ('748','%s ended with the winner %s.');
-INSERT INTO `mangos_string` (`entry`,`content_default`) VALUES ('749','No BattleGround found.');
-INSERT INTO `mangos_string` (`entry`,`content_default`) VALUES ('750','Following BattleGrounds found:\n%s.');
--- src/bindings/ScriptDev2/scripts/zone/wailing_caverns/def_wailing_caverns.h
+++
@@ -1,4 +0,0 @@
-#ifndef DEF_WAILING_CAVERNS_H
-#define DEF_WAILING_CAVERNS_H
-
-#endif 
