ec0a3afb5814a1775a619b3314c9ab2fca266fa6
Thyros <trainzfreak@gmx.de>
2009-06-18 12:55:53 +0200

    - [65] Fix a Typo which caused Flame shock to be not consumed by lava burst.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1233,7 +1233,7 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
             // assisting case, healing and resurrection
             if(unit->hasUnitState(UNIT_STAT_ATTACK_PLAYER))
                 m_caster->SetContestedPvP();
-            if( unit->isInCombat() && !(m_spellInfo->AttributesEx & SPELL_ATTR_EX3_NO_INITIAL_AGGRO) )
+            if( unit->isInCombat() && !(m_spellInfo->AttributesEx3 & SPELL_ATTR_EX3_NO_INITIAL_AGGRO) )
             {
                 m_caster->SetInCombatState(unit->GetCombatTimer() > 0);
                 unit->getHostilRefManager().threatAssist(m_caster, 0.0f);
