e46d45f5984354688482d9e4959ecdead996437e
Sanzzes <stevenbohm@sanzzes-city.com>
2009-06-18 17:29:25 +0200

    - Cleanup

--- sql/cwcore_sql/REV01_170609/mangos_reputation_example.sql
+++
@@ -1,8 +0,0 @@
--- Loken Heroic
-INSERT INTO `creature_onkill_reputation` VALUES (31538, 1037, 1052, 7, 0, 250, 7, 0, 250, 1);
--- Loken Normal
-INSERT INTO `creature_onkill_reputation` VALUES (28923, 1037, 1052, 7, 0, 50, 7, 0, 50, 1);
--- Keristrasza heroic
-INSERT INTO `creature_onkill_reputation` VALUES (30540, 1037, 1052, 7, 0, 250, 7, 0, 250, 1);
--- Keristrasza normal should not give any reputation
--- INSERT INTO `creature_onkill_reputation` VALUES (26723, 1037, 1052, 7, 0, 50, 7, 0, 50, 1);
