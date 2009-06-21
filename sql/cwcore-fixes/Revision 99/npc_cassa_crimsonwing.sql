e3b222eaa09d218cdb626f45d0e76cd25202f3e4
Thyros <trainzfreak@gmx.de>
2009-06-21 19:14:41 +0200

    - [99] Add Support for Quest 11142.

---
+++ sql/cwcore-fixes/Revision 99/npc_cassa_crimsonwing.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `ScriptName` = 'npc_cassa_crimsonwing' WHERE `entry` =23704;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`) VALUES 
+(42316, 2079, 11142, 1, 0, 0, 1101, 2, 1);
