5360f0d904e52e350d8fbe6950ae367532b11318
Thyros <trainzfreak@gmx.de>
2009-06-21 10:50:24 +0200

    - [94] Fix Glyph of Death Grip.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6783,6 +6783,14 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, Aura* triggeredB
             }
             case SPELLFAMILY_DEATHKNIGHT:
             {
+                // Glyph of Death Grip
+                if (trigger_spell_id == 58628)
+                {
+                    // remove cooldown of Death Grip
+                    if (GetTypeId()==TYPEID_PLAYER)
+                        ((Player*)this)->RemoveSpellCooldown(82);
+                    return true;
+                }
                 // Acclimation
                 if (auraSpellInfo->SpellIconID == 1930)
                 {
--- src/mangosd/Main.cpp
+++ src/mangosd/Main.cpp
@@ -156,7 +156,7 @@ extern int main(int argc, char **argv)
     sLog.outTitle( "//******* /********//****** //****** /***  //******");
     sLog.outTitle( " ///////  ////////  //////   //////  ///    ////// ");
     sLog.outTitle( "Based on MaNGOS - Modify by Thyros, Filipper, Sanzzes.");
-    sLog.outString( "[UECore-Revision]: 93\n\n");
+    sLog.outString( "[UECore-Revision]: 94\n\n");
 
     sLog.outString("Using configuration file %s.", cfg_file);
 
